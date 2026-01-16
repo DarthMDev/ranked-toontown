36f01f948b7ef5c9cb61729de8bd0ac55980919f
Kitt Kitt Lou <thetycoon90@gmail.com>
2026-01-16 08:10:11 +0400

    nuke build/scripts - deprecated

--- build/scripts/start_server_astron.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Toontown Ranked - Astron Server
-cd game\astron
-
-:launch
-astrond --loglevel info config/astrond.yml
-pause
-goto :launch
--- build/scripts/start_server_astron.sh
+++
@@ -1,7 +0,0 @@
-cd game/astron
-
-while true;
-do
-    ./astrond --loglevel info config/astrond.yml
-    sleep 5 
-done
