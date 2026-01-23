4e5e8490a84e99d1b67b7f80bd782e6bda18ea6a
clue <101487379+2clue@users.noreply.github.com>
2026-01-22 20:04:39 -0500

    ui: boarding group schmoved to the right + gui debugging

--- toontown/ui/UIHelpers.py
+++ toontown/ui/UIHelpers.py
@@ -6,14 +6,6 @@ def px_to_scale(asset_width, asset_height, window_width=1920, window_height=1080
 
     return (x_scale, 1, z_scale)
 
-def fontpx_to_scale(asset_width, asset_height, window_width=1920, window_height=1080): #yeah ok panda3d
-
-    aspect_ratio = window_width/window_height
-    x_scale = (asset_width/window_width) * (2.0 * aspect_ratio)
-    z_scale = (asset_height/window_height) * 2.0
-
-    return (x_scale, z_scale, 1)
-
 def coords_to_pos(x_coord, z_coord, window_width=1920, window_height=1080):
 
     aspect_ratio = window_width/window_height
@@ -22,5 +14,18 @@ def coords_to_pos(x_coord, z_coord, window_width=1920, window_height=1080):
 
     return (x_panda, 0, z_panda)
 
-print(px_to_scale(18, 13.8462, 308, 24))
-print(coords_to_pos(260.128, 0, 308, 24))
+def coords_to_a2dRightCenter_pos(x_coord, z_coord, window_width=1920, window_height=1080):
+    #calculate midpoint and aspect ratio
+    x_canvas_midpoint = window_width/2.0
+    aspect_ratio = window_width/window_height
+
+    #find x coord, subtract by midpoint because we're placing on the right side of the window,
+    #then divide by canvas midpoint to find the %. subtract it from 1 then mult by aspect ratio/window size whatever
+    x_panda = (1.0 - ((x_coord - x_canvas_midpoint)/x_canvas_midpoint)) * aspect_ratio
+
+    #z remains the same because it is parented to a center already (i think) but if i make methods for corners just use those instead
+    z_panda = 1.0 - ((z_coord/window_height) * 2.0)
+
+    #okay lowkey this might not even needed, just place it wherever and wrtReparentTo, might matter for backing/boilerplate/whatever
+
+    return (-x_panda, 0, z_panda)
